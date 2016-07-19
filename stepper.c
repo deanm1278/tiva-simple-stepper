683eec0d4fc7ed3a3852e101db7e6eb220755b1e
dean <deanm1278@gmail.com>
2016-07-19 17:32:53 -0400

    in progress

--- stepper.c
+++ stepper.c
@@ -19,7 +19,6 @@ static const uint8_t step_table[] = {
   0x06
 };
 
-
 void stepper_init(struct stepper *s){
     GPIOPinTypeOutput(s->AIN1_BASE, s->AIN1_PIN);
     GPIOPinTypeOutput(s->AIN2_BASE, s->AIN2_PIN);
@@ -27,19 +26,67 @@ void stepper_init(struct stepper *s){
     GPIOPinTypeOutput(s->BIN2_BASE, s->AIN2_PIN);
     
     s->stepping_rate = 0;
+    s->stepping_rate_target = 0;
+    
+    s->dir = 1;
+    
+    s->max_start_speed = 1000;
+    s->acc_max = 10000;
     
 }
-void stepper_disable(struct stepper *s);
+void stepper_disable(struct stepper *s){
+    GPIOPinWrite(s->AIN1_BASE, s->AIN1_PIN, 0x00);
+    GPIOPinWrite(s->AIN2_BASE, s->AIN2_PIN, 0x00);
+    GPIOPinWrite(s->BIN1_BASE, s->BIN1_PIN, 0x00);
+    GPIOPinWrite(s->BIN2_BASE, s->BIN2_PIN, 0x00);
+}
+
 void stepper_enable(struct stepper *s);
-void stepper_stop(struct stepper *s);
-void stepper_start(struct stepper *s);
 
-void stepper_set_stepping_rate(struct stepper *s, uint32_t stepping_rate){
-	TimerLoadSet(s->TIMER_BASE, s->TIMER, SysCtlClockGet()/stepping_rate);
-	s->stepping_rate = stepping_rate;
+void stepper_tick(struct stepper *s){
+    if(step_table[s->t_val] & AIN1_BIT){ GPIOPinWrite(s->AIN1_BASE, s->AIN1_PIN, s->AIN1_PIN); }
+    else{ GPIOPinWrite(s->AIN1_BASE, s->AIN1_PIN, 0x00); }
+    
+    if(step_table[s->t_val] & AIN2_BIT){ GPIOPinWrite(s->AIN2_BASE, s->AIN2_PIN, s->AIN2_PIN); }
+    else{ GPIOPinWrite(s->AIN2_BASE, s->AIN2_PIN, 0x00); }
+    
+    if(step_table[s->t_val] & BIN1_BIT){ GPIOPinWrite(s->BIN1_BASE, s->BIN1_PIN, s->BIN1_PIN); }
+    else{ GPIOPinWrite(s->BIN1_BASE, s->BIN1_PIN, 0x00); }
+    
+    if(step_table[s->t_val] & BIN2_BIT){ GPIOPinWrite(s->BIN2_BASE, s->BIN2_PIN, s->BIN2_PIN); }
+    else{ GPIOPinWrite(s->BIN2_BASE, s->BIN2_PIN, 0x00); }
+    
+    s->t_val = s->t_val + s->dir;
+    if(s->t_val > 3 || s->t_val < -3)
+        s->t_val = 0;
+    
+    stepper_set_stepping_rate(s, s->target_stepping_rate);
 }
 
-void stepper_set_dir(struct stepper *s, int dir){
-	s->dir = dir;
+void stepper_set_stepping_rate(struct stepper *s, int target) {
+    uint32_t max_delta, delta;
+
+    if (s->stepping_rate != stepping_rate) {
+        s->target_stepping_rate = stepping_rate;
+        delta = abs(s->target_stepping_rate - s->stepping_rate);
+
+        //set actual rate to the max allowed by acc rate
+        max_delta = acc_max / (1 << abs(s->stepping_rate));
+
+        delta = STEPPER_MIN(delta, max_delta);
+        if (s->target_stepping_rate < s->stepping_rate)
+            s->stepping_rate = s->stepping_rate - delta;
+        else
+            s->stepping_rate = s->stepping_rate + delta;
+        
+        s->dir = (s->stepping_rate < 0 ? -1 : 1);
+        TimerLoadSet(s->TIMER_BASE, s->TIMER, SysCtlClockGet() / (1 << abs(s->stepping_rate)));
+    }
 }
+
+
 void stepper_move_steps(struct stepper *s, uint32_t steps);
+
+void stepper_set_acc_max(struct stepper *s, uint32_t acc){
+    s->acc_max = acc;
+}
--- stepper.h
+++ stepper.h
@@ -4,11 +4,18 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-struct stepper {
-	uint32_t 			stepping_rate;	//steps per second
+#define AIN1_BIT 0x08
+#define AIN2_BIT 0x04
+#define BIN1_BIT 0x02
+#define BIN2_BIT 0x01
 
-	uint32_t			t_val;
-	int					dir;
+ #define STEPPER_MIN(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+struct stepper {
+//set these when you create the struct before calling stepper_init
         uint32_t                        AIN1_BASE;
         uint32_t                        AIN2_BASE;
         uint32_t                        BIN1_BASE;
@@ -19,16 +26,27 @@ struct stepper {
         uint32_t                        BIN2_PIN;
 	uint32_t			TIMER_BASE;
 	uint32_t			TIMER;
+        
+//no need to mess with these, use setters and getters
+        int     			stepping_rate;	//current steps per second
+        int                             stepping_rate_target; //target stepping rate
+        int                             dir;
+        uint32_t                        pos;
+	uint32_t			t_val;
+        uint32_t                        acc_max;
 };
 
 
 void stepper_init(struct stepper *s);
 void stepper_disable(struct stepper *s);
 void stepper_enable(struct stepper *s);
-void stepper_stop(struct stepper *s);
-void stepper_start(struct stepper *s);
-void stepper_set_stepping_rate(struct stepper *s, uint32_t stepping_rate);
-void stepper_set_dir(struct stepper *s, int dir);
+void stepper_tick(struct stepper *s);
+
+//set target speed in steps per second
+void stepper_set_stepping_rate(struct stepper *s, int stepping_rate);
 void stepper_move_steps(struct stepper *s, uint32_t steps);
+
+//set max acceleration rate in steps per second per second
+void stepper_set_acc_max(struct stepper *s, uint32_t acc);
 
 #endif /* STEPPER_H_ */
